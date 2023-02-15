c8561752cd5340e19c8e8a60f6e5fe9f5f4bc44c
Garikai Jenje <garikaijen@gmail.com>
2023-02-15 20:25:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <button
+        className="temp-trigger-modal-button"
+        onClick={() => setShowModal(true)}
+      >
+        Open Modal
+      </button>
+      {showModal && (
+        <React.Fragment>
+          <div
+            className="temp-modal-backdrop"
+            onClick={() => setShowModal(false)}
+          ></div>
+          <div className="temp-modal">
+            <div className="temp-modal-header">
+              <span>Title</span>
+              <span
+                className="temp-modal-header-close-button"
+                onClick={() => setShowModal(false)}
+              >
+                X
+              </span>
+            </div>
+            <div className=""></div>
+            <div class="temp-modal-player-container">
+              <img
+                src="https://plchldr.co/i/500x250"
+                className="temp-modal-player-image"
+                alt="temp-player"
+              />
+            </div>
+            <div className="temp-modal-controls">
+              <div className="temp-modal-controls-item">
+                <img
+                  className="temp-modal-controls-item-img"
+                  src="https://plchldr.co/i/500x250"
+                  alt="plchldr.co"
+                />
+              </div>
+              <div className="temp-modal-controls-item">
+                <img
+                  className="temp-modal-controls-item-img"
+                  src="https://plchldr.co/i/500x250"
+                  alt="plchldr.co"
+                />
+              </div>
+              <div className="temp-modal-controls-item">
+                <img
+                  className="temp-modal-controls-item-img"
+                  src="https://plchldr.co/i/500x250"
+                  alt="plchldr.co"
+                />
+              </div>
+              <div className="temp-modal-controls-item">
+                <img
+                  className="temp-modal-controls-item-img"
+                  src="https://plchldr.co/i/500x250"
+                  alt="plchldr.co"
+                />
+              </div>
+              <div className="temp-modal-controls-item">
+                <img
+                  className="temp-modal-controls-item-img"
+                  src="https://plchldr.co/i/500x250"
+                  alt="plchldr.co"
+                />
+              </div>
+              <div className="temp-modal-controls-item">
+                <img
+                  className="temp-modal-controls-item-img"
+                  src="https://plchldr.co/i/500x250"
+                  alt="plchldr.co"
+                />
+              </div>
+            </div>
+            <div className="temp-modal-buttons-container">
+              <button className="temp-modal-button temp-cancel-button">
+                CANCEL
+              </button>
+              <button className="temp-modal-button temp-ok-button">OK</button>
+            </div>
+          </div>
+        </React.Fragment>
+      )}
+    </React.Fragment>
   );
 }
-
-export default App;
